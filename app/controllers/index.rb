42feb6dbfeb643fc868bbe13d230cc6fffe5ce0d
Elaine Tsai <et.elainetsai@gmail.com>
2013-08-27 11:33:52 -0700

    first commit

---
+++ app/controllers/index.rb
@@ -0,0 +1,23 @@
+get '/' do
+  puts "[LOG] Getting /"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :index
+end
+
+get '/cool_url' do
+  puts "[LOG] Getting /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  @input = params[:input]
+
+  erb :get_cool_url
+end
+
+post '/cool_url' do
+  puts "[LOG] Posting to /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  @input = params[:input]
+  @story = params[:textarea]
+  erb :post_cool_url
+end
+
+
